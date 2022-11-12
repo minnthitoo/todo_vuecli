93d0c7d1eb891761b65c0cfbaf62f2abac2a0b3e
minnthitoo <mto.forlearning@gmail.com>
2022-11-12 09:42:20 +0630

    final

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
+  <div>
     <h1 class="text-center bg-primary text-white p-3">
-      <font-awesome-icon icon="fa-solid fa-user-secret" />{{ title }}'s' Todo
+      {{ title }}'s Todo
       List
     </h1>
     <div class="container">
-      <div class="row bg-danger">
-        
-        <div class="col fs-3">Tasks<font-awesome-icon icon="fa-solid fa-house" /></div>
-        <div class="col-2 fs-3">Done</div>
-      </div>
-      <div class="row mt-2" v-for="(task, index) in filterTask" :key="index">
-        <div class="col" v-bind:class="task.done ? 'delete' : ''">
-          {{ task.title }}
-        </div>
-        <div class="col-2">
-          <input type="checkbox" v-model="task.done" />
-        </div>
-      </div>
       <div class="row mt-3">
-        <div class="col bg-danger text-center text-white p-2">
-          <h5>Hide completed tasks</h5>
-          <input type="checkbox" v-model="completed"/>
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            v-model="newTask"
+            v-on:keyup.enter="add()"
+            placeholder="New Tesk"
+            aria-label="New Tesk"
+            aria-describedby="button-addon2"
+          />
+          <button
+            class="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+            @click="add()"
+          >
+            Add
+          </button>
         </div>
+      </div>
+      <div class="" v-if="filterTask.length > 0">
+        <div class="row">
+          <div class="col fs-3">
+            Tasks
+          </div>
+          <div class="col-2 fs-3">Done</div>
+        </div>
+        <div class="row mt-2" v-for="(task, index) in filterTask" :key="index">
+          <div class="col" v-bind:class="task.done ? 'delete' : ''">
+            {{ task.title }}
+          </div>
+          <div class="col-2">
+            <input type="checkbox" v-model="task.done" />
+          </div>
+        </div>
+        <div class="row mt-3">
+          <div class="col bg-danger text-center text-white p-2">
+            <h5>Hide completed tasks</h5>
+            <input type="checkbox" v-model="completed" />
+          </div>
+        </div>
+      </div>
+      <div class="" v-else>
+        <div class="alert alert-warning">There is no Data</div>
       </div>
     </div>
   </div>
@@ -34,33 +61,25 @@ export default {
   data: () => ({
     title: "CODE LAB",
     completed: false,
-    tasks: [
-      {
-        title: "Buy new Phone",
-        done: true,
-      },
-      {
-        title: "Buy new Computer",
-        done: true,
-      },
-      {
-        title: "Buy new Car",
-        done: false,
-      },
-      {
-        title: "Buy new Aeroplane",
-        done: false,
-      },
-      {
-        title: "Buy new satellite",
-        done: false,
-      },
-    ],
+    newTask: "",
+    tasks: [],
   }),
   computed: {
-    filterTask(){
+    filterTask() {
       return this.completed ? this.tasks.filter((v) => !v.done) : this.tasks;
-    }
+    },
+  },
+  methods: {
+    add() {
+      if (this.newTask == "") {
+        return alert("Enter some word");
+      }
+      this.tasks.push({
+        title: this.newTask,
+        done: false,
+      });
+      this.newTask = "";
+    },
   },
 };
 </script>
